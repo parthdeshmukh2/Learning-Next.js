0c158e0009c4180b196cac49cd4bc54b4cabd30c
Parth <parthdeshmukh99@gmail.com>
2024-03-03 22:12:24 +0530

    login and signup page added with UI

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+type User = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+export default function Login() {
+  const [user, setUser] = useState<User>({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const onLogin = () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>{loading ? "Processing" : "Login"}</h1>
+      <hr />
+
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        onClick={onLogin}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        Login here
+      </button>
+      <Link href="/signup">Visit Signup page</Link>
+    </div>
+  );
+}
